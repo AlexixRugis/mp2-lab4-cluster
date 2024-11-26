a2975d257fe2353a1b6b9e1f9ec133d370c6fe5e
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2024-11-26 11:26:35 +0300

    project structure

---
+++ src/queue.cpp
@@ -0,0 +1,5 @@
+#include "queue.h"
+
+int test(int a, int b) {
+    return a + b;
+}
---
+++ test/test_queue.cpp
@@ -0,0 +1,7 @@
+#include "queue.h"
+#include "gtest/gtest.h"
+
+TEST(QueueTest, test_test)
+{
+    EXPECT_EQ(test(1, 2), 3);
+}
